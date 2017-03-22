2739baf350d322b03a5966837e4d910501c08464
Administrator <Administrator@ACADEMY-6211VRH>
2017-03-22 16:47:18 +0100

    new for-loop

--- BraProgrammet/Program.cs
+++ BraProgrammet/Program.cs
@@ -17,6 +17,11 @@ namespace BraProgrammet
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine("Heh" + i);
+
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(" 0 ");
+                }
             }
         }
     }
